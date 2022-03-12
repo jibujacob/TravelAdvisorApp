9cfdaa51bfac14834cf2d6eac695e98ac33fbf05
jibujacob <jibupjacob@gmail.com>
2022-03-12 19:25:59 +0530

    Header Skeleton Structure with material UI completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+import {CssBaseline,Grid} from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
+
 function App() {
   return (
-    <div className="app">
-      Hello World
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Header/>
+        <Grid container spacing={3} style={{width:'100%'}}>
+          <Grid item xs={12} md={4}>
+            <List/>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Map/>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
+    </>
   );
 }
 
